c4aa060c8c5c5648ee524d7ef581c1e2bd09e240
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-06 06:39:21 -0800

    script that prepares a MySQL server for the project

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- script that prepares a MySQL server for the project
+-- database hbnb_test_db
+-- new user hbnb_test (in localhost)
+-- password  set to hbnb_test_pwd
+-- hbnb_test should have all privileges on the database hbnb_test_db
+-- hbnb_test should have SELECT privilege on the database performance_schema
+-- if the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
