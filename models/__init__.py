73da5ee0b68f9d3a7086ec2eb4ba608df7b0ce9d
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-07 11:24:28 -0800

    Executable files

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+import os
 
-
-storage = FileStorage()
-storage.reload()
+store = 'HBNB_TYPE_STORAGE'
+if store in os.environ.keys() and os.environ['HBNB_TYPE_STORAGE'] == 'db':
+    storage = DBStorage()
+    storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 import models
+import os
 
 Base = declarative_base()
 
