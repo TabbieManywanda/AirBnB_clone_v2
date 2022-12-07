905a34405250831bb935a86425463bb4d17760cc
Lewis Safari <lleewwii1@gmail.com>
2022-12-07 20:42:16 +0300

    fix

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,6 +7,6 @@
 -- if the database hbnb_test_db or the user hbnb_test already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
