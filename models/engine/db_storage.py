e86ed0a621f13a8ced7b0e9d8c70fa48015d5bff
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-07 17:59:14 -0800

    edit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.scoping import scoped_session
 from models import *
 import os
+from models.base_model import Base
 
 
 class DBStorage:
@@ -17,13 +18,13 @@ class DBStorage:
 
     def __init__(self):
         '''Create engine'''
-        self.__engine = create_engine('mysql+mysqldb://' +
-                                       os.environ['HBNB_MYSQL_USER'] + 
-                                       ':' + os.environ['HBNB_MYSQL_PWD'] + 
-                                       '@' + os.environ['HBNB_MYSQL_HOST'] + 
-                                       ':3306/' + 
-                                       os.environ['HBNB_MYSQL_DB'], 
-                                       pool_pre_ping=True)
+        self.__engine = create_engine(
+                'mysql+mysqldb://' + os.environ['HBNB_MYSQL_USER'] +
+                ':' + os.environ['HBNB_MYSQL_PWD'] +
+                '@' + os.environ['HBNB_MYSQL_HOST'] +
+                ':3306/' +
+                os.environ['HBNB_MYSQL_DB'],
+                pool_pre_ping=True)
 
         try:
             if os.environ['HBNB_ENV'] == "test":
@@ -68,4 +69,5 @@ class DBStorage:
     def reload(self):
         '''create all tables in the database'''
         Base.metadata.create_all(self.__engine)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
+        self.__session = scoped_session(
+                sessionmaker(bind=self.__engine, expire_on_commit=False))
