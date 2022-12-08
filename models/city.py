1b47147dfe2330f2b7be678a52246d3daf878334
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-07 18:57:20 -0800

    Edit

--- models/city.py
+++ models/city.py
@@ -13,8 +13,8 @@ if store in os.environ.keys() and os.environ['HBNB_TYPE_STORAGE'] == 'db':
         """ The city class, contains state ID and name """
 
         __tablename__ = 'cities'
-        name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
         places = relationship('Place', backref='cities', cascade='all, delete')
 
         def __init__(self, **kwargs):
--- models/state.py
+++ models/state.py
@@ -14,7 +14,8 @@ if store in os.environ.keys() and os.environ['HBNB_TYPE_STORAGE'] == 'db':
 
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship(
+                "City", backref="state", cascade='all, delete')
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
