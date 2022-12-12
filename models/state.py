e96cd22adca915c6f8f4d56e12e77e984e5aec6c
Lewis Safari <lleewwii1@gmail.com>
2022-12-12 11:52:06 +0300

    test

--- models/state.py
+++ models/state.py
@@ -7,26 +7,21 @@ import models
 from sqlalchemy.orm import relationship, backref
 import os
 
-store = 'HBNB_TYPE_STORAGE'
-if store in os.environ.keys() and os.environ['HBNB_TYPE_STORAGE'] == 'db':
-    class State(BaseModel, Base):
-        """ State class """
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+            "City", backref="state", cascade='all, delete')
 
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-                "City", backref="state", cascade='all, delete')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-else:
-    class State(BaseModel):
-        name=""
-        @property
-        def cities(self):
-            all_cities = models.storage.all("City")
-            tmp = []
-            for x in all_cities:
-                if all_cities[x].state_id == self.id:
-                    tmp.append(all_cities[x])
-            return tmp
+    @property
+    def cities(self):
+        all_cities = models.storage.all("City")
+        tmp = []
+        for x in all_cities:
+            if all_cities[x].state_id == self.id:
+                tmp.append(all_cities[x])
+        return tmp
