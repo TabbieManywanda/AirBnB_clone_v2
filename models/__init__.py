b38d089addc5fca75c75917250217e68b9a23576
TabbieManywanda <achiengtabitha@gmail.com>
2022-12-13 08:05:38 -0800

    Edit

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
 
+
 store = getenv("HBNB_TYPE_STORAGE")
 if store == 'db':
     storage = DBStorage()
