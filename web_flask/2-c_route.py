fcde885585aa76096ef03c5a1d1cd597107c8bb1
safarilewis <lleewwii1@gmail.com>
2023-01-14 11:45:12 +0300

    c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Starts a flask application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>',strict_slashes=False)
+def c_is_fun():
+    return 'C %s' % escape(text.replace('_',' '))
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
