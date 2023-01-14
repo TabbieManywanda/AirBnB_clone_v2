bdfa6dc2667842dd900d68de05274ecff154ff8f
safarilewis <lleewwii1@gmail.com>
2023-01-14 12:10:16 +0300

    python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Starts a flask application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Returns Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>',strict_slashes=False)
+def c_is_fun(text):
+    """Returns C followed by the value of text"""
+    return 'C %s' %text.replace('_', ' ')
+
+
+@app.route('/python/<text>',strict_slashes=False)
+def python_is_fun(text):
+    """Returns python followed by the value of text"""
+    if not len(text):
+        text = "is cool"
+    return 'Python %s' %text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
