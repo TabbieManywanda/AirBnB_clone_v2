d3435449d3159537944608cc23e3fa32bc79c656
safarilewis <lleewwii1@gmail.com>
2023-01-14 11:53:24 +0300

    some changes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 
 @app.route('/c/<text>',strict_slashes=False)
 def c_is_fun():
-    return 'C %s' % escape(text.replace('_',' '))
+    return 'C %s' %text.replace('_',' ')
 
 
 if __name__ == '__main__':
