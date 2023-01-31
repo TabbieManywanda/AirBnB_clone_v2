1805efafdea86749043e33f2d75f1856dde60952
TabbieManywanda <achiengtabitha@gmail.com>
2023-01-31 13:53:08 -0800

    Edit

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import *
+from models.city import City
 from sqlalchemy import Column, Integer, String
 import models
 from sqlalchemy.orm import relationship, backref
